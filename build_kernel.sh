0d386319d470db041399dd989616caad7ff72f85
rasgaxota572 <rasgaxota575@gmail.com>
2025-10-22 10:32:13 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,5 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r 
 export ARCH=arm64
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
-make exynos9830-x1slte_defconfig
+make extreme_r8s_defconfig
 make
