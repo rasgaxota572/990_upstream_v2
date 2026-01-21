77e4ee8eaa982a839607b10d4d96cdbbbe64b77c
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-21 08:07:25 +0100

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -148,12 +148,6 @@ EXPORT_SYMBOL(vfs_getattr);
  *
  * 0 will be returned on success, and a -ve error code if unsuccessful.
  */
-
-#ifdef CONFIG_KSU_SUSFS
-extern bool ksu_init_rc_hook __read_mostly;
-extern void ksu_handle_sys_newfstatat(int fd, loff_t *kstat_size_ptr);
-#endif // #ifdef CONFIG_KSU_SUSFS
-
 int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 		 u32 request_mask, unsigned int query_flags)
 {
@@ -167,11 +161,6 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 	if (f.file) {
 		error = vfs_getattr(&f.file->f_path, stat,
 				    request_mask, query_flags);
-#ifdef CONFIG_KSU_SUSFS
-		if (unlikely(ksu_init_rc_hook)) {
-			ksu_handle_sys_newfstatat(fd, &stat->size);
-		}
-#endif // #ifdef CONFIG_KSU_SUSFS
 		fdput(f);
 	}
 	return error;
