228fe4fefbedc14de04742810f7fa433c9d4e68e
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-02-01 20:07:05 +0100

    try

--- fs/exec.c
+++ fs/exec.c
@@ -1733,7 +1733,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_execveat_hook __read_mostly;
 extern bool ksu_su_compat_enabled __read_mostly;
-extern bool susfs_is_boot_completed_triggered __read_mostly;
+extern bool susfs_is_sdcard_android_data_decrypted __read_mostly;
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
@@ -1762,7 +1762,7 @@ static int __do_execve_file(int fd, struct filename *filename,
 		goto orig_flow;
 	}
 
-	if (unlikely(ksu_execveat_hook || !susfs_is_boot_completed_triggered)) {
+	if (unlikely(ksu_execveat_hook || !susfs_is_sdcard_android_data_decrypted)) {
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	} else if ((__ksu_is_allow_uid_for_current(current_uid().val))) {
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
