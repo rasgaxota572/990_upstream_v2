17a6057c7d17d9441d6163c090d135a625e43f7d
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-02-06 00:18:03 +0100

    Update susfs_def.h

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -1,7 +1,6 @@
 #ifndef KSU_SUSFS_DEF_H
 #define KSU_SUSFS_DEF_H
 
-#include <linux/version.h>
 #include <linux/bits.h>
 
 /********/
@@ -76,46 +75,6 @@
 #define ND_FLAGS_LOOKUP_LAST		0x2000000
  
 #define MAGIC_MOUNT_WORKDIR "/debug_ramdisk/workdir"
-
-/* From KernelSU */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
-typedef const struct qstr *susfs_fname_t;
-#define susfs_fname_len(f) ((f)->len)
-#define susfs_fname_arg(f) ((f)->name)
-#else
-typedef const unsigned char *susfs_fname_t;
-#define susfs_fname_len(f) (strlen(f))
-#define susfs_fname_arg(f) (f)
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
-#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
-int name(struct fsnotify_mark *mark, u32 mask, struct inode *inode,    \
-struct inode *dir, const struct qstr *file_name, u32 cookie)
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
-#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
-int name(struct fsnotify_group *group, struct inode *inode, u32 mask,  \
-const void *data, int data_type, susfs_fname_t file_name,       \
-u32 cookie, struct fsnotify_iter_info *iter_info)
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 18, 0)
-#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
-int name(struct fsnotify_group *group, struct inode *inode, u32 mask,  \
-const void *data, int data_type, susfs_fname_t file_name,       \
-u32 cookie, struct fsnotify_iter_info *iter_info)
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
-#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
-int name(struct fsnotify_group *group, struct inode *inode,            \
-struct fsnotify_mark *inode_mark,                             \
-struct fsnotify_mark *vfsmount_mark, u32 mask,                \
-const void *data, int data_type, susfs_fname_t file_name,       \
-u32 cookie, struct fsnotify_iter_info *iter_info)
-#else
-#define SUSFS_DECL_FSNOTIFY_OPS(name)                                            \
-int name(struct fsnotify_group *group, struct inode *inode,            \
-struct fsnotify_mark *inode_mark,                             \
-struct fsnotify_mark *vfsmount_mark, u32 mask, void *data,    \
-int data_type, susfs_fname_t file_name, u32 cookie)
-#endif
 
 static inline bool susfs_is_current_proc_umounted(void) {
 	return test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
