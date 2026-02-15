7a702fc8a264e4ff754a1c89f1de2c90c454e821
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-02-15 12:57:25 +0100

    Update susfs.c

--- fs/susfs.c
+++ fs/susfs.c
@@ -1029,8 +1029,8 @@ static int watch_one_dir(struct watch_dir *wd)
  */
 static SUSFS_DECL_FSNOTIFY_OPS(susfs_handle_sdcard_inode_event)
 {
-	if (!file_name || file_name->len != 7 ||
-	    memcmp(file_name->name, "Android", 7))
+	if (!file_name || strlen(file_name) != 7 ||
+		memcmp(file_name, "Android", 7))
 		return 0;
 
 	if (test_and_set_bit(0, &sdcard_cleanup_scheduled))
