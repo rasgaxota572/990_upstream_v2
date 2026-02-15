c1413ed0770d0a8cb064a94d3e9545d2169cb89d
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-02-15 18:34:34 +0100

    Update task_mmu.c

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1793,7 +1793,6 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 		if (ret)
 			goto out_free;
 		ret = walk_page_range(start_vaddr, end, &pagemap_walk);
-		up_read(&mm->mmap_sem);
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 		vma = find_vma(mm, start_vaddr);
 		if (vma && vma->vm_file) {
@@ -1803,6 +1802,7 @@ static ssize_t pagemap_read(struct file *file, char __user *buf,
 			}
 		}
 #endif
+		up_read(&mm->mmap_sem);
 		start_vaddr = end;
 
 		len = min(count, PM_ENTRY_BYTES * pm.pos);
